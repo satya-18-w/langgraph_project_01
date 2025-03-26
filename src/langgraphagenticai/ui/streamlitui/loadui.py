06acfadd10a11981ac1f7324392f538d66836975
satya-18-w <satyajitsamal198076@gmail.com>
2025-03-26 21:00:06 +0530

    make more changes in ui

--- src/langgraphagenticai/ui/streamlitui/loadui.py
+++ src/langgraphagenticai/ui/streamlitui/loadui.py
@@ -35,7 +35,8 @@ class LoadStreamlitUI:
     def load_streamlit_ui(self):
         st.set_page_config(page_title= self.config.get_page_title(),layout="wide")
         #st.set_page_config(page_title="🤖 "+ self.config.get_page_title(),layout="wide")
-        st.header("🤖 " + self.config.get_page_title())
+        # st.header("🤖 " + self.config.get_page_title())
+        st.header(self.config.get_page_title())
         st.session_state.timeframe=""
         st.session_state.IsFetchButtonClicked=False
         st.session_state.IsSDLC=False
